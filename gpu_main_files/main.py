352ad6fda9d164589bc59d878f5ea3223d8dae51
apastinante <anibalpastinante@gmail.com>
2024-10-09 20:00:53 +0200

    Fixes and cleaning up

--- gpu_main_files/main.py
+++ gpu_main_files/main.py
@@ -10,18 +10,10 @@ from skopt import gp_minimize
 from skopt.plots import plot_gaussian_process 
 
 # Home-made functions and classes
+
 from utils_opt.func_utils import *
 # from scipy.optimize import minimize_scalar
 from utils_opt.drawnow import ProgramDefOpt, tolSetter
-# import logging 
-# logging.basicConfig(
-#     filename="opt.log",
-#     encoding="utf-8",
-#     filemode="a",
-#     format="%(asctime)s - %(levelname)s - %(message)s",
-#     level=logging.NOTSET,
-#     datefmt="%Y-%m-%d %H:%M:%S",
-# )
 
 
 #BLonD imports
@@ -29,13 +21,11 @@ import blond.utils.bmath as bm
 from blond.beam.beam import Beam, Proton
 from blond.beam.distributions import matched_from_distribution_function
 from blond.beam.profile import CutOptions, Profile
-from blond.impedances.impedance import (InducedVoltageFreq, InductiveImpedance,
-                                        TotalInducedVoltage)
-from blond.impedances.impedance_sources import InputTable
 from blond.input_parameters.rf_parameters import RFStation
-from blond.input_parameters.ring import Ring,RingOptions
-
+from blond.input_parameters.ring import Ring
 from blond.trackers.tracker import FullRingAndRF,RingAndRFTracker
+
+
 
 def moving_average(x, w):
     return np.convolve(x, np.ones(w), 'valid') / w
@@ -101,15 +91,16 @@ def range_comp(v1, ring):
     return t_vals, stable_phase
 
 def obj_func_plotter(xs,means,stds,evals,turns):
-    # Plot the the objective function at every turn in a colorplot
-    
+    # Plot the the objective function at every sampled turn in a colorplot
+    plt.close('all')
+
     fig = plt.figure()
     
 
     ax = fig.add_subplot(111)
     
 
-    p = ax.scatter(xs, turns, means, c=means, cmap='plasma',linewidth = 0, antialiased=False)
+    p = ax.scatter(xs, turns, c=means, cmap='viridis',linewidth = 0, antialiased=False)
     # obs = ax.plot(evals,turns, 'k.')
     
     ax.set_xlabel('Phase [rad]')
@@ -118,7 +109,7 @@ def obj_func_plotter(xs,means,stds,evals,turns):
 
     fig1 = plt.figure()
     ax1 = fig1.add_subplot(111)
-    s = ax.scatter(xs, turns, stds, c=stds, cmap='magma',linewidth = 0, antialiased=False)
+    s = ax1.scatter(xs, turns, c=stds, cmap='viridis',linewidth = 0, antialiased=False)
 
     ax1.set_xlabel('Phase [rad]')
     ax1.set_ylabel('Turn number')
@@ -128,7 +119,6 @@ def obj_func_plotter(xs,means,stds,evals,turns):
     fig.savefig('./gpu_output_files/EX_02_fig/obj_func_mean.png')
     fig1.savefig('./gpu_output_files/EX_02_fig/obj_func_std.png')
 
-    plt.show()
 
 
 # Define workers 
@@ -262,12 +252,10 @@ ref_slice_beam.n_macroparticles = y
 
 interp_ref_beam = comp_prof([ref_slice_beam.bin_centers, ref_slice_beam.n_macroparticles])
 
-# I need a function that will compute the synchronous phase 
-# and detect when the rate of change is very large and make 
-# individual ranges of dt for the optimization to cater to
-# the 
+
 
 # Initialize the 2nd reference RFStation for computing the array
+
 ref_RF_section = RFStation(ring, [1], (voltages[0]), (sync_momentum[0], np.pi*np.ones_like(sync_momentum[0])), 1)
 ref_voltage = ref_RF_section.voltage[0]
 
@@ -275,15 +263,12 @@ t_valss , phi_s1 =  range_comp(ref_voltage, ring)
 
 turn_vals = [np.where(ring.cycle_time <= t)[0][-1] for t in t_valss]
 
-
 n_slices = np.round(ring.t_rev[0] / width_bin).astype(int)
 
 t_end = ring.cycle_time[-1]
-# t_opt =  np.arange(0.01, t_arr[-1]+ dt_opt, dt_opt)
 start_turn = 0
 sim_arr = []
 
-# for iter in 
 
 t_arr = []
 dummy = 1
@@ -399,21 +384,17 @@ for i, entry in enumerate(sim_arr):
         Objects, phase2_arr = run_simulation(entry, None, ring, None, None, sync_momentum, phi_val, init = True, voltages=voltages)
         phis.append(phi_val)
         init_phi_est = phi_val
-        # print('Phase at t = 0 ms is: ', phi_val.x)
         
         print('{x:2f} of the beam is alive'.format(x=Objects[0].beam.n_macroparticles_alive/Objects[0].beam.n_macroparticles))
 
-    # Main loop goes here
     else:
         print(entry)
         obj_func =  partial(comp_obj2, interp_ref_beam, entry, [t_arr[i], t_arr[i-1]],ring, phase2_arr[1],Objects, sync_momentum, init = False, unique = True)
 
-        # logging.debug(f'{key} : {getsizeof(a[key])}')
-        if i<=3:
-        # phi_val = minimize_scalar(obj_func ,bounds = [0, np.pi*2], method = 'bounded', tol = 1e-1)
+        if i<=3: # Use the calculated stable phase
             phi_val = gp_minimize(obj_func, [(phi_s1[entry[2]-1] -0.4*np.pi, np.clip(phi_s1[entry[2]-1] +0.4*np.pi,a_min=None, a_max=2*np.pi))], x0= [init_phi_est], n_calls=max_iter, verbose=True, acq_func="PI" , n_random_starts=max_iter//5)## print(obj_func.value)
 
-        else:
+        else: # Use the previous value as the center-point
             phi_val = gp_minimize(obj_func, [(np.clip(phis[-1] -0.4*np.pi, a_min=0, a_max=None), np.clip(phis[-1] +0.4*np.pi, a_max=2*np.pi, a_min=None))], x0= [init_phi_est], n_calls=max_iter, verbose=True, acq_func="PI" , n_random_starts=max_iter//5)## print(obj_func.value)
 
                 #gp_minimize(obj_func, [(0, np.pi*1.05)],n_calls=max_iter, n_random_starts=5,random_state=1234) 
@@ -422,6 +403,7 @@ for i, entry in enumerate(sim_arr):
         # Get the probed values
         evals.append(np.vstack((np.array(phi_val.x_iters).flatten() , phi_val.func_vals)))
 
+        # Get the model values for the objective function and its standard deviation
         x_i = np.linspace(phi_val.space.dimensions[0].bounds[0] , phi_val.space.dimensions[0].bounds[1],1000)
         x_model_i = phi_val.space.dimensions[0].transform(x_i)
         x_model_i = x_model_i.reshape(-1,1)
@@ -431,18 +413,13 @@ for i, entry in enumerate(sim_arr):
         std.append(std_i)
         xs.append(x_i)
 
+        # Run the simulation to save the objects
         Objects, phase2_arr = run_simulation(entry, [t_arr[i], t_arr[i-1]], ring, phase2_arr[1], Objects, sync_momentum, phi_val.x[0], init = False )
         phis.append(phi_val.x[0])
         init_phi_est = phi_val.x[0]
         print('{x:2f} percent of the beam is alive'.format(x=Objects[0].beam.n_macroparticles_alive/Objects[0].beam.n_macroparticles*100))
 
-        # print('Phase at t = ', t_arr[i]*1000, ' ms is: ', phi_val.x)
         
-        
-        
-        if i == 6:
-            #get out of the loop and plot
-            break
     
 
 
@@ -453,9 +430,6 @@ turn_plots = np.zeros_like(means)
 for i, entry in enumerate(sim_arr):
     if i ==0: 
         pass 
-    elif i == 6:
-        #get out of the loop and plot
-        break
     else:
         turn_plots[i,:] = entry[2]
     
@@ -469,6 +443,9 @@ obj_func_plotter(xs.flatten(),means.flatten(),std.flatten(),evals,turn_plots.fla
 
 
 # Save the phase array 
+np.save('mean_obj.npy', means)
+np.save('std_obj.npy', std)
+np.save('x_obj.npy', xs)
 np.save('phase_array.npy', np.array([phase2_arr]))
 np.save('t_arr.npy' , t_arr)
 
